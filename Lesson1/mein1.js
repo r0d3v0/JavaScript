d1f3c49e9edca37b6e951d9477ab520daf8653f1
r0d3v0 <yd3vonuk@gmail.com>
2023-11-20 23:36:54 +0200

    finish

--- Lesson1/mein1.js
+++ Lesson1/mein1.js
@@ -1,7 +1,65 @@
- let str= 'hello';
-console.log(str)
+console.log('-----1.1------');
 
+let str1 = 'hello';
+ console.log(str1)
 
+ let str2 = 'owu';
+ console.log(str2)
 
-'owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+ let str3 = 'com';
+ console.log(str3)
 
+ let str4 = 'ua';
+ console.log(str4)
+
+ let numb1 = 1;
+ console.log(numb1)
+
+ let numb2 = 10;
+ console.log(numb2)
+
+ let numb3 = -999;
+ console.log(numb3)
+
+ let numb4 = 123;
+ console.log(numb4)
+
+ let numb5 = 3.14;
+ console.log(numb5)
+
+ let numb6 = 2.7;
+ console.log(numb6)
+
+ let numb7 = 16;
+ console.log(numb7)
+
+ let bol1 = true;
+ console.log(bol1)
+
+ let bol2 = false;
+ console.log(bol2)
+
+ console.log('-----1.2------');
+
+let firstName = "Elon";
+let middleName = "Musk";
+let lastName = "Petrovich";
+console.log(firstName + ' ' + middleName + ' ' + lastName);
+
+console.log('-----1.3------');
+
+let a = 100; let b = '100'; let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log('-----1.4------');
+
+let firstName4 = prompt("Введіть ваше ім'я")
+let middleName4 = prompt("Введіть ваше Прізвище")
+let age4 = prompt("Введіть ваш вік")
+
+console.log(firstName4)
+console.log(middleName4)
+console.log(age4)
