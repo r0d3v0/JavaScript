ca44a7789f599195dbfc7ab24dc6adcc008a69f9
r0d3v0 <yd3vonuk@gmail.com>
2023-11-25 18:39:12 +0200

    HW2 finish

---
+++ Lesson2/script/mein2.js
@@ -0,0 +1,9 @@
+let time = 59
+if (time > 45 && time <= 59) {
+    console.log('time:4/4')
+} else if (time > 30 && time <= 45) {
+    console.log('time:3/4')
+} else if (time > 15 && time <= 30) {
+    console.log('time:2/4')
+} else if (time > 0 && time <= 15) {
+    console.log('time:1/4')}
